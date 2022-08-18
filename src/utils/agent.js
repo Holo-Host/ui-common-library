622eb6603e2b88b4ac6d20b57662aa94a4125676
Jost Schulte <jost.schulte@protonmail.com>
2022-08-18 16:21:23 -0600

    refactor(hash)!: replace Buffer by js-base64

--- src/utils/agent.js
+++ src/utils/agent.js
@@ -1,23 +1,8 @@
+import {Base64} from "js-base64";
+
 export const agentIdRegex = /^uhCAk([A-Za-z0-9_\-]{48})$/
 
 export const decodeAgentId = base64AgentId =>
-  new Uint8Array(Buffer.from(base64AgentId.slice(1), 'base64'))
+  Base64.toUint8Array(base64AgentId)
 
-export const encodeAgentId = agentIdBuffer => {
-  const base64 = Buffer.from(agentIdBuffer).toString('base64')
-  let urlSafeBase64 = ''
-  for (let i = 0; i < base64.length; i++) {
-    let char = base64.charAt(i)
-    if (char === '/') {
-      char = '_'
-    } else if (char === '+') {
-      char = '-'
-    }
-    urlSafeBase64 += char
-  }
-  if (urlSafeBase64.charAt(0) !== 'u') {
-    return 'u' + urlSafeBase64
-  } else {
-    return urlSafeBase64
-  }
-}
+export const encodeAgentId = agentIdBuffer => `u${Base64.fromUint8Array(agentIdBuffer, true)}`
