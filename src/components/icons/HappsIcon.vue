fc0cae412e259c7ea4375ec257ba9bc30b8babaa
patrwinfspam+github@gmail.com <patrwinfspam+github@gmail.com>
2022-10-25 08:58:03 -0600

    fix fill color on happsicon

--- src/components/icons/HappsIcon.vue
+++ src/components/icons/HappsIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <Icon class='icon' :fill="white">
+  <Icon class='icon' :fill="color">
     <path d="M19.3432 4H4.65684C3.74179 4 3 4.74179 3 5.65684V10.3432C3 11.2582 3.74179 12 4.65684 12H19.3432C20.2582 12 21 11.2582 21 10.3432V5.65684C21 4.74179 20.2582 4 19.3432 4Z" fill="none" :stroke="color" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     <path d="M19.3432 12H4.65684C3.74179 12 3 12.7418 3 13.6568V18.3432C3 19.2582 3.74179 20 4.65684 20H19.3432C20.2582 20 21 19.2582 21 18.3432V13.6568C21 12.7418 20.2582 12 19.3432 12Z" fill="none" :stroke="color" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     <path d="M7 8V8.01" :stroke="color" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
