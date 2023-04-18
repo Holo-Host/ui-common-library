2c89f76d4c08d8edf5e91822975a5e725f1ad2b4
patrwinfspam+github@gmail.com <patrwinfspam+github@gmail.com>
2023-04-18 11:54:59 -0600

    add console.logs

--- src/stores/useHolochainStore.js
+++ src/stores/useHolochainStore.js
@@ -61,7 +61,9 @@ const makeUseHolochainStore = ({ installed_app_id, app_ws_url }) => defineStore(
         throw new Error('Tried to make a zome call before storing appInfo')
       }
   
-      const cellId = this.appInfo.cell_info[role_name]?.Provisioned?.cell_id 
+      console.log(`🦠 callZome cell_info (role_name: ${role_name})`, this.appInfo.cell_info)
+      console.log(`🦠 callZome holofuel cell_info (role_name: ${role_name})`, this.appInfo.cell_info[role_name])
+      const cellId = this.appInfo.cell_info[role_name]?.Provisioned?.cell_id
 
       if (!cellId) {
         throw new Error(`Couldn't find provisioned cell with role_name ${role_name}`)
