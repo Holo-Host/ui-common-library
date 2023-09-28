2eb81762487e1f21fa01a2fa8364243f11b1043b
Patrick Winfield <patrick.winfield@holo.host>
2023-09-28 08:24:42 -0600

    left chevron icon

---
+++ src/components/icons/LeftChevronIcon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Icon from './Icon.vue'
+</script>
+
+<template>
+  <Icon
+    fill="#606C8B"
+    class="icon"
+  >
+    <path d="M4.94.94L1.89 4l3.05 3.06L4 8 0 4l4-4 .94.94z" />
+  </Icon>
+</template>
+
+<style scoped>
+.icon {
+  width: 8px;
+  height: 8px;
+}
+</style>
