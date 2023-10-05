f124476b753326dd393cd452c500c040a6dfe185
Patrick Winfield <Paterick@users.noreply.github.com>
2023-10-04 18:02:53 -0600

    B06022 Add common icons & add optional slot for hApp card view image (#70)
    * left chevron icon
    
    * fix left chevron
    
    * add chain & clock icons
    
    * wrap happ card image with slot

---
+++ src/components/icons/ClockIcon.vue
@@ -0,0 +1,53 @@
+<template>
+    <Icon
+      stroke="#313C59"
+      fill="transparent"
+      class="icon"
+    >
+      <path
+        d="M10 17.5a7.5 7.5 0 100-15 7.5 7.5 0 000 15z"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      /><path
+        d="M10 5.8V10l2.5 2.5"
+        stroke="#313C59"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </Icon>
+  </template>
+  
+  <style scoped>
+  .icon {
+    width: 20px;
+    height: 20px;
+  }
+  </style>
+    
+<script>
+
+import Icon from './Icon.vue'
+
+export default {
+name: 'ClockIcon',
+components: {
+    Icon
+},
+props: {
+    disabled: {type: Boolean, default: false },
+    color: {
+    type: String,
+    default: "#606C8B"
+    }
+},
+computed: {
+    fillOpacity () {
+    return this.disabled ? '0.18' : '1'
+    }
+} 
+}
+</script>
+
+
---
+++ src/components/icons/LeftChevronIcon.vue
@@ -0,0 +1,36 @@
+<template>
+  <Icon :fill="color" class='icon' viewbox="0 0 8 8">
+    <path :fill-opacity="fillOpacity" d="M4.94.94L1.89 4l3.05 3.06L4 8 0 4l4-4 .94.94z" />
+  </Icon>
+</template>
+
+<script>
+
+import Icon from './Icon.vue'
+
+export default {
+  name: 'LeftChevronIcon',
+  components: {
+    Icon
+  },
+  props: {
+    disabled: {type: Boolean, default: false },
+    color: {
+      type: String,
+      default: "#606C8B"
+    }
+  },
+  computed: {
+    fillOpacity () {
+      return this.disabled ? '0.18' : '1'
+    }
+  } 
+}
+</script>
+
+<style scoped>
+.icon {
+  width: 8px;
+  height: 8px;
+}
+</style>
