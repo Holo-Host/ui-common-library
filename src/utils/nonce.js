82dfd337793c7f33e95063fca09f8d6752b3b5d3
Patrick Winfield <patrick.winfield@holo.host>
2024-01-11 15:18:41 -0700

    fix nonce for headless browser

--- src/utils/nonce.js
+++ src/utils/nonce.js
@@ -1,7 +1,19 @@
 export const generateB64Nonce = () => {
     const nonceLength = 16;
     const nonceArray = new Uint8Array(nonceLength);
-    window.crypto.getRandomValues(nonceArray);
+
+    if (window.crypto === undefined || window.crypto.getRandomValues === undefined) {
+        const crypto = require('crypto');
+
+        Object.defineProperty(globalThis, 'crypto', {
+        value: {
+            getRandomValues: arr => crypto.randomBytes(arr.length)
+        }
+        })
+    } else {
+        window.crypto.getRandomValues(nonceArray);
+    }
+    
 
     // Encode the nonce in Base64
     const base64Nonce = window.btoa(String.fromCharCode(...nonceArray));
