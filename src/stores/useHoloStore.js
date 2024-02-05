a39c440ff81970178258348cc9ea5969a74f9048
Lisa Jetton <ldjetton21@gmail.com>
2024-02-04 18:00:49 -0600

    Refactor/sl api param (#88)
    * update happ_id casing
    
    * update happ_ids when signing payload
    
    * fix happ_ids param
    
    * make sig happ_id a string;
    
    * parse happ_ids for url
    
    * add log
    
    * make paylaod order dependeant
    
    ---------
    
    Co-authored-by: Patrick Winfield <patrick.winfield@holo.host>

--- src/stores/useHoloStore.js
+++ src/stores/useHoloStore.js
@@ -133,7 +133,10 @@ const makeUseHoloStore = ({ connectionArgs, MockWebSdk }) => defineStore('holo',
       return hAppStatistics
     },
     async fetchAllHAppStats(happIds, days, environment, serviceLogPort) {
-      const payload = generateServiceLogPayload({ "happIds": happIds, "days": days.toString() })
+      // NB: The happ_ids param needs to be a string to succeed the msgpack serialization
+      // and validation check in the server.
+      const happIdsString = JSON.stringify(happIds)
+      const payload = generateServiceLogPayload({ "days": days.toString(), "happ_ids": happIdsString })
       const { _, signature  } = await client.signPayload(payload)
       const hAppStatistics = await allHappStats(payload, signature, this.agentKey, environment, serviceLogPort)
 
