1b136d39a87c58ae897901d66b864aa76cf0ddc1
Patrick Winfield <patrick.winfield@holo.host>
2024-03-21 16:03:26 -0600

    remove console log

--- src/stores/useHbsStore.js
+++ src/stores/useHbsStore.js
@@ -21,7 +21,6 @@ const makeUseHbsStore = ({ useHoloStore }) => {
             
                 const { _, signature  } = await await useHoloStore().signPayload(payload)
                 const kycLevel = await fetchAgentKycLevel(payload, signature, environment, hbsServicePort)
-                console.log(`🚁 HBS loadAgentKycLevel KYC level: ${kycLevel}`)
                 this.kycLevel = kycLevel
                 return kycLevel
             },
