0156e379633e6f87bb6b71484769d79140cfe4fd
Patrick Winfield <patrick.winfield@holo.host>
2024-07-14 13:50:13 -0600

    uncomment dropdown and fix compile err

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <!-- <div class="label" v-if="label && label.length > 0">{{ label }}:</div>
-    <select v-model="selectedOption" :disabled="disabled" class="drop-down" @change="$emit('optionChanged', selectedOption)">
+    <div class="label" v-if="label && label.length > 0">{{ label }}:</div>
+    <select :value="selectedOption" :disabled="disabled" class="drop-down" @change="updateSelection($event.target.value)">
       <option v-for="option in options" :value="option" :key="option">
         {{ option }}
       </option>
-    </select> -->
+    </select>
   </div>
 </template>
 
@@ -18,6 +18,11 @@ export default {
     label: String,
     options: Array,
     selectedOption: String
+  },
+  methods: {
+    updateSelection(value) {
+      this.$emit('optionChanged', value);
+    }
   }
 }
 </script>
