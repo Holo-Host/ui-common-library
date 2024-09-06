957c2ac3d37cda13c698ce1de28216937a715b12
Zeeshan Abid <zeeshan.abid.com@gmail.com>
2024-09-06 10:23:35 +0100

    authenticateAgent missing from imports

--- src/stores/useHbsStore.js
+++ src/stores/useHbsStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { registrationFetchJurisdictions } from '../services/hbs'
+import { registrationFetchJurisdictions, authenticateAgent } from '../services/hbs'
 
 const makeUseHbsStore = ({ useHoloStore }) => {
     return defineStore('hbs', {
